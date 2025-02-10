27b547c2fce7cb67a74c1f6bf1d1493f85d597eb
granzer04 <abdel.94@live.com>
2025-02-10 02:39:31 +0000

    modified AWS s3 bucket - emptying when destroy

--- aws-TF/backend.tf
+++ aws-TF/backend.tf
@@ -19,6 +19,39 @@ resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
   }
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "my_bucket_lifecycle" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  rule {
+    id     = "delete-old-versions"
+    status = "Enabled"
+
+    expiration {
+      expired_object_delete_marker = true
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 1
+    }
+  }
+}
+
+resource "null_resource" "empty_s3_bucket" {
+  provisioner "local-exec" {
+    command = <<EOT
+      aws s3 rm s3://${aws_s3_bucket.my_bucket.bucket} --recursive
+      versions_json=$(aws s3api list-object-versions --bucket ${aws_s3_bucket.my_bucket.bucket} --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}' --output json)
+      if [ "$(echo $versions_json | jq '.Objects | length')" -gt 0 ]; then
+        echo $versions_json | aws s3api delete-objects --bucket ${aws_s3_bucket.my_bucket.bucket} --delete file://-
+      fi
+    EOT
+  }
+
+  triggers = {
+    bucket_name = aws_s3_bucket.my_bucket.id
+  }
+}
+
 resource "aws_dynamodb_table" "my_table" {
   name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
